fb69751fd4b6c6f834fdb5bb4ec6587949e779fa
Ashish Juyal <ashish@thecodecamp.in>
2024-03-20 01:23:19 +0530

    lab-02 checked in

---
+++ labs/02-lab/main.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+/**
+TODO: Find abstractions and compose interfaces to fix the commented code
+*/
+
+// A saving account can be used to deposit and withdraw
+type SavingsAccount struct {
+	AccountNumber int
+	Balance       float64
+}
+
+func (s *SavingsAccount) Deposit(amount float64) {
+	s.Balance += amount
+}
+
+func (s *SavingsAccount) Withdraw(amount float64) {
+	if s.Balance >= amount {
+		s.Balance -= amount
+	} else {
+		fmt.Println("Insufficient balance.")
+	}
+}
+
+func (s *SavingsAccount) GenerateStatement() {
+	fmt.Printf("Savings Account Statement\nAccount Number: %d\nBalance: $%.2f\n", s.AccountNumber, s.Balance)
+}
+
+// A fixed deposit account is not withdrawable
+type FixedDepositAccount struct {
+	AccountNumber int
+	Balance       float64
+}
+
+func (s *FixedDepositAccount) Deposit(amount float64) {
+	s.Balance += amount
+}
+
+func (s *FixedDepositAccount) GenerateStatement() {
+	fmt.Printf("Fixed deposit Account Statement\nAccount Number: %d\nBalance: $%.2f\n", s.AccountNumber, s.Balance)
+}
+func main() {
+	// Create instances of both accounts
+	savings := &SavingsAccount{AccountNumber: 123, Balance: 1000}
+	fd := &FixedDepositAccount{AccountNumber: 456, Balance: 2000}
+
+	_ = savings // TODO: remove line, this is only to get rid of compilation error (unused variables)
+	_ = fd      // TODO: remove line, this is only to get rid of compilation error (unused variables)
+
+	// manageSavingsAccount(savings) //TODO: fix this
+
+	// manageFixedDepositAccount(fd) //TODO: fix this
+}
+
+/*
+// TODO: should be able to deposit, withdraw and print statements
+func manageSavingsAccount(as ?) {
+}
+
+// TODO: should be able to deposit and print statements
+func manageFixedDepositAccount(as ?) {
+}
+*/
