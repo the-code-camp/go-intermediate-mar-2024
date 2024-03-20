9e8b9072166daa6b8d0149ddef3eee4f90843b7f
Ashish Juyal <ashish@thecodecamp.in>
2024-03-21 02:55:37 +0530

    anonymous functions

---
+++ instructor/day-1/hof/02-anonymous.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	func() {
+		fmt.Println("anonymous fn invoked")
+	}()
+
+	// pass values to anonymous functions
+	func(x, y int) {
+		result := x + y
+		fmt.Println("Result: ", result)
+	}(100, 200)
+
+	fmt.Println(func(x, y int) int {
+		return x + y
+	}(100, 200))
+}
