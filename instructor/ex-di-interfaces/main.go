55e39a40cd8cee562ad11b613f6149e6e6b155ac
Ashish Juyal <ashish@thecodecamp.in>
2024-03-22 00:33:33 +0530

    structs json encoding

--- instructor/ex-di-interfaces/main.go
+++ instructor/ex-di-interfaces/main.go
@@ -10,17 +10,27 @@ import (
 	"net/http"
 )
 
+type Employee struct {
+	Id     int     `json:"id"`
+	Name   string  `json:"employee_name"`
+	Salary float32 `json:"-"`
+}
+
+//{"Id":100,"Name":"emp-1","Salary":1234.56}
+//{"id":100,"employee_name":"emp-1","salary":1234.56}
+
 func main() {
 
 	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
-		m := make(map[string]string, 0)
-		m["message"] = "Hello World!"
-
+		// m := make(map[string]string, 0)
+		// m["message"] = "Hello World!"
 		// w.Write([]byte("Hello World!"))
 
-		if err := json.NewEncoder(w).Encode(m); err != nil {
+		emp := Employee{100, "emp-1", 1234.56}
+
+		if err := json.NewEncoder(w).Encode(emp); err != nil {
 			// send a bad response code
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("Some error occoured"))
