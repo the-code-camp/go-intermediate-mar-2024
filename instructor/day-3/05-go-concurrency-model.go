1a9c20a24e062921a69b63def74aa2cb48cdb1e3
Ashish Juyal <ashish@thecodecamp.in>
2024-03-22 20:48:55 +0530

    concurrency model

---
+++ instructor/day-3/05-go-concurrency-model.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// Its a semaphone based counter
+
+func main() {
+	count := flag.Int("count", 0, "Number of go routines to spin up")
+	flag.Parse()
+	fmt.Printf("%d number of goroutings staring.. Press ENTER\n", *count)
+	fmt.Scanln()
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < *count; i++ {
+		wg.Add(1)
+		go f1(i, wg)
+	}
+
+	// waiting here till the counter becomes zero
+	wg.Wait()
+	fmt.Println("All goroutines finished. Press ENTER to complete")
+	fmt.Scanln()
+}
+
+func f1(id int, wg *sync.WaitGroup) {
+	// decrement the counter
+	defer wg.Done()
+	fmt.Printf("fn[%d] invoked\n", id)
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	fmt.Printf("fn[%d] completed\n", id)
+}
